c4b8cdf4a3e83228601ea298ba3ba20c7c79f733
antonioudakis <t.antonioudakis@gmail.com>
2021-01-15 22:11:18 +0200

    upload a file from a form

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 
-class Home(TemplateView)
+class Home(TemplateView):
 	template_name = 'home.html'
 
-def upload(request)
+def upload(request):
+	context = {}
+	if request.method == 'POST':
+		uploaded_file = request.FILES['document']
+		fs = FileSystemStorage()
+		name = fs.save(uploaded_file.name, uploaded_file)
+		context['url'] = fs.url(name)
+	return render(request, 'dashboard/upload.html', context)
